480543ab3e35b34488b51cdff0d243a1bd96507a
Ömer Üstün <omerberatustun@gmail.com>
2023-11-01 22:51:11 +0300

    Implement asynchronous file deletion with logging and error handling
    This commit introduces several enhancements to the 'order-66' utility:
    
    - Transition to asynchronous execution using 'Tokio' for non-blocking sleep during file deletion scheduling.
    - Utilize the 'anyhow' crate for robust error handling, providing context for any file operation failures.
    - Implement structured logging with the 'tracing' crate, replacing standard 'println!' output for better visibility and maintenance, especially when running as a background service.
    - Minor refactoring to accommodate async patterns and improve code readability.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
 use clap::Parser;
-use std::fs;
-use std::thread;
-use std::time::Duration;
+use tokio::fs;
+use tokio::time::Duration;
+use anyhow::{Context, Result};
+use tracing::{info, error, Level};
+use tracing_subscriber;
 
 #[derive(Parser)]
 struct Opts {
-    /// Path to the file to be deleted
     #[clap(short, long)]
     file_path: String,
     
-    /// Time in minutes after which the file will be deleted
     #[clap(short, long)]
     time_in_minutes: u64,
 }
 
-fn main() {
-    // Parse the command line arguments using clap
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .init();
+
     let opts: Opts = Opts::parse();
 
-    // Extract file path and time in minutes
     let file_path = &opts.file_path;
     let time_in_minutes = opts.time_in_minutes;
 
-    // Calculate the duration in milliseconds
     let duration = Duration::from_secs(time_in_minutes * 60);
 
-    println!("Scheduled to delete '{}' in {} minutes.", file_path, time_in_minutes);
+    info!("Scheduled to delete '{}' in {} minutes.", file_path, time_in_minutes);
 
-    // Sleep for the given duration
-    thread::sleep(duration);
+    tokio::time::sleep(duration).await;
 
-    // Attempt to delete the file
-    match fs::remove_file(file_path) {
-        Ok(_) => println!("'{}' has been deleted.", file_path),
-        Err(e) => println!("Error deleting '{}': {}", file_path, e),
-    }
+    fs::remove_file(file_path)
+        .await
+        .with_context(|| format!("Failed to delete file: '{}'", file_path))?;
+
+    info!("'{}' has been deleted.", file_path);
+    Ok(())
 }
