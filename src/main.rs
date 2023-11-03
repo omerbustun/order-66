580f1fb54cef00dba7963004d5a83367f64c7d52
Ömer Üstün <omerberatustun@gmail.com>
2023-11-03 23:57:02 +0300

    Implement task status updates and concurrency control
    This commit introduces several key enhancements and fixes:
    
    - Added a new `Failed` status for tasks that encounter errors during deletion.
    - Utilized `Arc<Mutex<Vec<_>>>` to safely share and update task statuses across threads.
    - Reorganized the main function logic to ensure task status updates are captured accurately after asynchronous operations.
    - The task summary now includes a count of failed tasks alongside pending, completed, expired, and cancelled.
    - Applied structured logging for better visibility of task status progression and error reporting.
    - Ensured all spawned async tasks are accounted for before saving task statuses, using a shared mutable vector protected by a mutex.
    - Task statuses are now updated after the completion of their respective async blocks, ensuring consistency in the `tasks.json` file.
    - Minor refactoring to enhance code clarity and maintainability.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use clap::Parser;
-use tokio::fs as async_fs;
 use anyhow::Result;
-use tracing::{info, error, Level};
-use tracing_subscriber;
 use chrono::{DateTime, Utc};
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
+use tokio::fs as async_fs;
+use tracing::{error, info, Level};
+use tracing_subscriber;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 enum TaskStatus {
     Pending,
     Completed,
     Expired,
     Cancelled,
+    Failed,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -29,7 +31,7 @@ struct DeletionTask {
 struct Opts {
     #[clap(short, long)]
     file_path: Option<String>,
-    
+
     #[clap(short, long)]
     time_in_minutes: Option<u64>,
 }
@@ -60,13 +62,15 @@ async fn main() -> Result<()> {
     let mut tasks = load_tasks()?;
 
     // Task summary
-    let (mut pending_count, mut completed_count, mut expired_count, mut cancelled_count) = (0, 0, 0, 0);
+    let (mut pending_count, mut completed_count, mut expired_count, mut cancelled_count, mut failed_count) =
+        (0, 0, 0, 0, 0);
     for task in &tasks {
         match task.status {
             TaskStatus::Pending => pending_count += 1,
             TaskStatus::Completed => completed_count += 1,
             TaskStatus::Expired => expired_count += 1,
             TaskStatus::Cancelled => cancelled_count += 1,
+            TaskStatus::Failed => failed_count += 1,
         }
     }
 
@@ -75,56 +79,81 @@ async fn main() -> Result<()> {
     info!("Completed: {}", completed_count);
     info!("Expired:   {}", expired_count);
     info!("Cancelled: {}", cancelled_count);
+    info!("Cancelled: {}", failed_count);
+
 
     if let (Some(file_path), Some(time_in_minutes)) = (opts.file_path, opts.time_in_minutes) {
         let delete_at = Utc::now() + chrono::Duration::seconds(time_in_minutes as i64 * 60);
-        info!("Scheduled to delete '{}' in {} minutes.", file_path, time_in_minutes);
-    
+        info!(
+            "Scheduled to delete '{}' in {} minutes.",
+            file_path, time_in_minutes
+        );
+
         let new_task = DeletionTask {
-            file_path: file_path.clone(), 
+            file_path: file_path.clone(),
             delete_at,
             created_at: Utc::now(),
             status: TaskStatus::Pending,
         };
-        
+
         tasks.push(new_task);
-        
+
         save_tasks(&tasks)?;
     }
 
-    for task in tasks.iter_mut() {
+    let tasks_status = Arc::new(Mutex::new(Vec::new()));
+
+    for task in tasks.iter() {
         match task.status {
             TaskStatus::Pending => {
                 let duration_until_delete = task.delete_at.signed_duration_since(Utc::now());
-                info!("Duration until delete: {:?}", duration_until_delete);
+                info!(
+                    "Duration until delete for '{}': {:?}",
+                    task.file_path, duration_until_delete
+                );
                 if duration_until_delete > chrono::Duration::zero() {
+                    // The task is pending and not expired
                     let file_path = task.file_path.clone();
-                    info!("Scheduling deletion for: {}", &file_path);
+                    let tasks_status = tasks_status.clone();
                     tokio::spawn(async move {
                         info!("Waiting to delete file: {}", &file_path);
                         tokio::time::sleep(duration_until_delete.to_std().unwrap()).await;
                         info!("Attempting to delete file: {}", &file_path);
-                        match async_fs::remove_file(&file_path).await {
-                            Ok(_) => {
-                                info!("'{}' has been deleted.", &file_path);
-                            },
-                            Err(e) => {
-                                error!("Failed to delete file '{}': {}", &file_path, e);
-                            },
+                        if async_fs::remove_file(&file_path).await.is_ok() {
+                            info!("'{}' has been deleted.", &file_path);
+                            let mut statuses = tasks_status.lock().unwrap();
+                            statuses.push((file_path, TaskStatus::Completed));
+                        } else {
+                            error!("Failed to delete file '{}'", &file_path);
+                            let mut statuses = tasks_status.lock().unwrap();
+                            statuses.push((file_path, TaskStatus::Failed));
                         }
                     });
                 } else {
-                    task.status = TaskStatus::Expired;
+                    info!(
+                        "Task for '{}' has expired and will not be processed.",
+                        task.file_path
+                    );
+                    let mut statuses = tasks_status.lock().unwrap();
+                    statuses.push((task.file_path.clone(), TaskStatus::Expired));
                 }
             }
             _ => {}
         }
     }
-    
+
+    tokio::signal::ctrl_c().await.expect("Failed to listen for ctrl_c");
+
+    {
+        let shared_statuses = tasks_status.lock().unwrap();
+        for (index, task) in tasks.iter_mut().enumerate() {
+            if let Some((_, status)) = shared_statuses.get(index) {
+                task.status = status.clone();
+            }
+        }
+    }
 
     save_tasks(&tasks)?;
 
-
-    tokio::signal::ctrl_c().await.expect("Failed to listen for ctrl_c");
     Ok(())
 }
